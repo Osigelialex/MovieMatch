1ceb83eb8fb63b6874b5adfea82e79a9ee6cd904
Osigelialex <osigelialex@gmail.com>
2023-08-04 01:22:18 +0100

    made slider work on mobile

--- static/home.js
+++ static/home.js
@@ -5,7 +5,7 @@ let startX, scrollLeft;
 
 let startDragging = function (e) {
   mouseDown = true;
-  startX = e.pageX - e.currentTarget.offsetLeft;
+  startX = e.type === 'touchstart' ? e.touches[0].pageX - e.currentTarget.offsetLeft : e.pageX - e.currentTarget.offsetLeft;
   scrollLeft = e.currentTarget.scrollLeft;
 };
 
@@ -16,15 +16,20 @@ let stopDragging = function (event) {
 const handleSliderMove = (e) => {
   e.preventDefault();
   if (!mouseDown) { return; }
-  const x = e.pageX - e.currentTarget.offsetLeft;
+  const x = e.type === 'touchmove' ? e.touches[0].pageX - e.currentTarget.offsetLeft : e.pageX - e.currentTarget.offsetLeft;
   const scroll = x - startX;
   e.currentTarget.scrollLeft = scrollLeft - scroll;
 };
 
 sliders.forEach((slider) => {
-  // Add the event listeners to each slider
+  // Add the event listeners to each slider for both touch and mouse events
   slider.addEventListener('mousedown', startDragging, false);
   slider.addEventListener('mouseup', stopDragging, false);
   slider.addEventListener('mouseleave', stopDragging, false);
   slider.addEventListener('mousemove', handleSliderMove);
+
+  slider.addEventListener('touchstart', startDragging, false);
+  slider.addEventListener('touchend', stopDragging, false);
+  slider.addEventListener('touchcancel', stopDragging, false);
+  slider.addEventListener('touchmove', handleSliderMove);
 });
